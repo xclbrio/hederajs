f88fb1ec0fe13830b40d974610ceb11edba571a5
Dmitry <45120847+D1mk4Novikov@users.noreply.github.com>
2019-03-20 00:52:41 +0300

    Added
    Added:
    * .flowconfig
    * .flowcoverage.json
    * package.json
    * JS_SDK_Flow/Main.js
    * settings.json

---
+++ JS_SDK_Flow/Main.js
@@ -0,0 +1,18 @@
+// This is the Main Class of JavaScript SDK for Hedera.
+
+
+// @flow
+
+
+
+// Main class
+function HederaJavaScriptSDK() {
+	let a: number;
+	let b: number;
+}
+
+
+
+HederaJavaScriptSDK.prototype.function = testFunc() {
+	
+}
