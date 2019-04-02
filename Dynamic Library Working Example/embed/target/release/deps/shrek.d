fead618dc40a9bd21658bd417c652eb8ceda1dd4
Dmitry <45120847+D1mk4Novikov@users.noreply.github.com>
2019-04-02 16:52:34 +0300

    Add
    Added:
    * target folder with .dylib file

---
+++ Dynamic Library Working Example/embed/target/release/deps/shrek.d
@@ -0,0 +1,5 @@
+/Users/d1mk4/Desktop/DDEC Projects/Rust Learning/Dynamic Library/embed/target/release/deps/libshrek.dylib: src/lib.rs
+
+/Users/d1mk4/Desktop/DDEC Projects/Rust Learning/Dynamic Library/embed/target/release/deps/shrek.d: src/lib.rs
+
+src/lib.rs:
---
+++ Dynamic Library Working Example/embed/target/release/libshrek.d
@@ -0,0 +1,1 @@
+/Users/d1mk4/Desktop/DDEC\ Projects/Rust\ Learning/Dynamic\ Library/embed/target/release/libshrek.dylib: /Users/d1mk4/Desktop/DDEC\ Projects/Rust\ Learning/Dynamic\ Library/embed/src/lib.rs
