31e391d3f4c582148c31062788cc7d4e00215101
Dmitry <45120847+D1mk4Novikov@users.noreply.github.com>
2019-04-02 18:29:39 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+// Connecting library to use foreign functions
+const NodeFFI = require("ffi");
+
+
+function HederaSDK(selectedTargetOS = 0) {
+	
+	// Importing a dynamic library with functions to work based on the selected OS
+	switch (selectedTargetOS) {
+		case 0:
+			var libraryPath = "./DynamicLibrary/Linux/lib.so";
+			break;
+		case 1:
+			var libraryPath = "./DynamicLibrary/macOS/lib.dylib";
+			break;
+		case 2:
+			var libraryPath = "./DynamicLibrary/Windows/lib.dll";
+			break;
+	}
+	
+	let importLibrary = NodeFFI.Library(libraryPath, {
+		// It is a place for functions from dynamic library
+		// Example
+		/*
+		 * "function_name": ["result_type", ["input_type1", ["input_type2"]]]
+		 */
+	});
+	
+}
+
+
+HederaSDK.prototype.getFile = function (/* _ */) {
+	// paste code here...
+}
+
+
+module.exports = HederaSDK;
